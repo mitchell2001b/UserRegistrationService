a53bb897056d8fe61c825f2acb050c1e5df8f9f0
mitchell2001b <79633852+mitchell2001b@users.noreply.github.com>
2023-10-12 13:02:23 +0200

    deleted unused test

--- src/test/java/com/semester6/UserRegistrationMicroService/AccountRegistrationMicroServiceApplicationTests.java
+++ src/test/java/com/semester6/UserRegistrationMicroService/AccountRegistrationMicroServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AccountRegistrationMicroServiceApplicationTests {
 
-	@Test
+	/*@Test
 	void contextLoads() {
-	}
+	}*/
 
 }
